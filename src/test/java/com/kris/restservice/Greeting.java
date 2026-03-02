78fc80bae666e9b493f3fe991bc82e80773d292a
Krispy125 <kburns19@rockets.utoledo.edu>
2026-03-01 19:13:06 -0500

    Added Greeting record class to eliminate error found in RestServiceApplication.java

---
+++ src/test/java/com/kris/restservice/Greeting.java
@@ -0,0 +1,5 @@
+package com.kris.restservice;
+
+public record Greeting(long id, String content) {
+    
+}
---
+++ src/test/java/com/kris/restservice/RestServiceApplication.java
@@ -0,0 +1,11 @@
+package com.kris.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+}
